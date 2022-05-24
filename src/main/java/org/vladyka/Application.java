ea44461a3f7c935fb568459f24a11526e297421c
Taras Vladyka <tarasvladyka@gmail.com>
2022-05-24 16:51:53 +0300

    Init commit

---
+++ src/main/java/org/vladyka/Application.java
@@ -0,0 +1,26 @@
+package org.vladyka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            log.info("Registering bot...");
+            telegramBotsApi.registerBot(new SimpleEchoBot());
+        } catch (TelegramApiRequestException e) {
+            log.info("Failed to register bot(check internet connection/bot token/make sure only one instance of bot is running).");
+            e.printStackTrace();
+        }
+        log.info("Telegram bot is ready to accept updates from user......");
+    }
+}
+
---
+++ src/main/java/org/vladyka/SimpleEchoBot.java
@@ -0,0 +1,56 @@
+package org.vladyka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class SimpleEchoBot extends TelegramLongPollingBot {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    /**
+     * This is an entry point for any messages, or updates received from user<br>
+     * Docs for "Update object: https://core.telegram.org/bots/api#update
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String textFromUser = update.getMessage().getText();
+
+            Long userId = update.getMessage().getFrom().getId();
+            String userFirstName = update.getMessage().getFrom().getFirstName();
+
+            log.info("[{}, {}] : {}", userId, userFirstName, textFromUser);
+
+            SendMessage sendMessage = SendMessage.builder()
+                    .chatId(update.getMessage().getChat().getId().toString())
+                    .text("Hello, I've received your text: " + textFromUser)
+                    .build();
+            try {
+                this.sendApiMethod(sendMessage);
+            } catch (TelegramApiException e) {
+                //just print stack trace for now
+                log.error("Exception when sending message: ", e);
+            }
+        } else {
+            log.warn("Unexpected update from user");
+        }
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        // username which you give to your bot bia BotFather (without @)
+        return "your_bot_username ";
+    }
+
+    @Override
+    public String getBotToken() {
+        // do not expose the token to the repository,
+        // always provide it externally(for example as environmental variable)
+        return "your_bot_token";
+    }
+}
